ffb160da5366ea766027143cd37de11ecd97f461
nickyxxxl <nickyswinckels@yahoo.com>
2024-04-03 17:42:41 +0200

    Upgrade system, backend

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
-import { IncrementalGame, BuildingType, UpgradeType } from "./game.ts";
+import { IncrementalGame, BuildingTypes, UpgradeTypes } from "./game.ts";
 
 const Game = new IncrementalGame(100);
 
 document.querySelector("#building-menu").addEventListener('click', (element) => {
     let tile_id = element.target.closest(".ship-grid").id.split('-')[1];
-    if (!Game.build(BuildingType.tree, tile_id)) {
+    if (!Game.build(BuildingTypes.tree, tile_id)) {
         return;
     }
     $( `#tile-${tile_id} .tile-empty`).replaceWith("<div onclick='ToggleUpgradesMenu(this)' class='tile-building tile-tree'></div>");
 })
 
 document.querySelector("#upgrade-menu").addEventListener('click', (element) => {
+    console.log(element);
     let tile_id = element.target.closest(".ship-grid").id.split('-')[1];
-    if (!Game.build(BuildingType.tree, tile_id)) {
+    if (!Game.upgrade(UpgradeTypes.speed_3,tile_id)) {
         return;
     }
     //$( `#tile-${tile_id} .tile-empty`).replaceWith("<div onclick='ToggleUpgradesMenu(this)' class='tile-building tile-tree'></div>");
@@ -21,4 +22,4 @@ document.querySelector("#upgrade-menu").addEventListener('click', (element) => {
 setInterval(() => {
     Game.updateBuildings();
     document.querySelector("#money-display").innerHTML = Game.getMoney()
-}, 500)
+}, 40)
--- src/game.ts
+++ src/game.ts
@@ -1,19 +1,20 @@
-export {IncrementalGame, BuildingType, UpgradeType};
+export {IncrementalGame, BuildingTypes, UpgradeTypes};
 
 // Enums
-enum BuildingType {
+enum BuildingTypes {
     none = 0,
-    tree,
+    tree = 100,
+    seed = 50,
 }
 
-enum UpgradeType {
+enum UpgradeTypes {
     none = 0,
-    speed_1,
-    speed_2,
-    speed_3,
-    efficiency_1,
-    efficiency_2,
-    efficiency_3,
+    speed_1 = 20,
+    speed_2 = 40,
+    speed_3 = 60,
+    efficiency_1 = 50,
+    efficiency_2 = 100,
+    efficiency_3 = 150,
 }
 
 class IncrementalGame {
@@ -31,13 +32,29 @@ class IncrementalGame {
     }
 
     // Construction
-    build(type: BuildingType, location: number): boolean {
-        if (this.money < 100) {
+    build(type: BuildingTypes, location: number): boolean {
+        if (this.money < type) {
             return false;
         }
-        this.buildings.push(new Building(type));
-        console.log(this.buildings);
-        this.money -= 100;
+        if (this.buildings[location] != null) {
+            return false
+        }
+        this.buildings[location] = new Building(type);
+        this.money -= type;
+        return true;
+    }
+
+    upgrade(type: UpgradeTypes, location: number): boolean {
+        if (this.money < type) {
+            return false;
+        }
+        console.log(this.buildings[location]);
+        
+        if (this.buildings[location].upgrades.includes(type)) {
+            return false;
+        }
+        this.buildings[location].upgrade(type);
+        this.money -= type;
         return true;
     }
 
@@ -50,20 +67,66 @@ class IncrementalGame {
 }
 
 class Building {
-    type: BuildingType;
-    upgrades: Array<UpgradeType>;
+    type: BuildingTypes;
+    upgrades: Array<UpgradeTypes>;
+
+    earnings: number;
+    multiplier: number;
+
+    cooldown: number;
+    temperature: number;
+    prevTime: number;
 
     constructor(building_type) {
         this.type = building_type;
+        this.cooldown = 1000;
+        this.temperature = 1000;
+        this.prevTime = Date.now();
+        this.upgrades = new Array;
+
+        switch (this.type) {
+            case BuildingTypes.tree:
+                this.earnings = 10;
+                this.multiplier = 1;
+                this.cooldown = 1000;
+                break;
+        }
     }
 
     // Install Upgrade
-    upgrade(upgrade) {
+    upgrade(upgrade: UpgradeTypes) {
         this.upgrades.push(upgrade);
+        switch (upgrade) {
+            case UpgradeTypes.speed_1:
+                this.cooldown = 800;
+                break;
+            case UpgradeTypes.speed_2:
+                this.cooldown = 400;
+                break;
+            case UpgradeTypes.speed_3:
+                this.cooldown = 200;
+                break;
+            case UpgradeTypes.efficiency_1:
+                this.multiplier = 2;
+                break;
+            case UpgradeTypes.efficiency_2:
+                this.multiplier = 3;
+                break;
+            case UpgradeTypes.efficiency_3:
+                this.multiplier = 5;
+                break;
+        }
     }
 
     // Get amount of resources produced
     getMoney(): number {
-        return 5;
+        this.temperature -= Date.now() - this.prevTime;
+        this.prevTime = Date.now()
+        
+        if (this.temperature < 0) {
+            this.temperature = this.cooldown;
+            return this.earnings * this.multiplier;
+        }
+        return 0
     }
 }
