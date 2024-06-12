d3815fe769eda38a9193d3275fb0e486dab8df12
Sten <vermeulen.sten@gmail.com>
2024-06-12 15:43:36 +0200

    added dodge game files

--- src/minigame_spinning.js
+++ src/minigame_spinning.js
@@ -3,3 +3,7 @@ import $ from 'jquery';
 window.$ = $;
 
 let Game = new IncrementalGame(100);
+
+let rotation = x/window.innerWidth;
+  //Make rotation in X and Y direction depend on mouse position/rotation
+  // Goal is to balance out ring
